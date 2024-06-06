97ec15b0fe5bede7acb412e2f26d4dddf713406b
alvpan <pan.alvanopoulos@gmail.com>
2024-06-06 14:17:15 +0300

    When see prices is clicked, latest data is selected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -345,6 +345,8 @@ export default function Home() {
 
   const handleSeePricesClick = async () => {
     clearHistoricalChartData();
+    setActiveHeaderButton('Latest Prices');
+    setChartVisible(false);
     if (activeHeaderButton === 'Latest Prices') {
       await fetchLatestPricesData();
     } else if (activeHeaderButton === 'Historical Data') {
