3ea435a9effd7afd400ee1d4c1ea364b036ac077
alvpan <pan.alvanopoulos@gmail.com>
2024-07-28 20:47:02 +0300

    use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect, useRef, CSSProperties } from "react";
 import dynamic from 'next/dynamic';
 import { ApexOptions } from 'apexcharts';
@@ -579,7 +581,7 @@ export default function Home() {
       }
     });
   };
-  
+
   return (
     <div className="main-container">
       <header className="header">
